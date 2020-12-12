3ed788c29c78c278bb1636315b527392e2890a65
Artyom <artyom7998@yandex.ru>
2020-12-12 18:51:25 +0300

    lesson-3

---
+++ Friends/FriendTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  FriendTableViewCell.swift
+//  GB_UserUI_ClientVK
+//
+//  Created by Артем Зарудный on 09.12.2020.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var friendCity: UILabel!
+    
+    /*
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    */
+    
+    
+}
---
+++ Groups/GroupsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  GroupsTableViewCell.swift
+//  GB_UserUI_ClientVK
+//
+//  Created by Артем Зарудный on 12.12.2020.
+//
+
+import UIKit
+
+class GroupsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    
+    /*
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+     */
+}
